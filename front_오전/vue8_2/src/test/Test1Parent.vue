3198ad68055893ad1fdf85dfbb3459cb127777cf
kxxheehxxn <fpqk7099@gmail.com>
2025-03-28 14:06:31 +0900

    0328 front 오전 vue 8, 9

---
+++ front_오전/vue8_2/src/test/Test1Parent.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'Test1Parent',
+  data() {
+    return {}
+  },
+}
+</script>
+
+<template>
+  <div class="test1parent">연습을 시작한다.</div>
+</template>
+
+<style scoped></style>
