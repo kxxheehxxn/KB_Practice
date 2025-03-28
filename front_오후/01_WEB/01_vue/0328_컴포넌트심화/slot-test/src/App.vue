aee4c2c47c20598d2d2dfcecb3638c8ab4e7a691
kxxheehxxn <fpqk7099@gmail.com>
2025-03-28 17:34:58 +0900

    0328 심화1까지

---
+++ front_오후/01_WEB/01_vue/0328_컴포넌트심화/slot-test/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import NoSlotTest from './components/NoSlotTest.vue';
+import SlotTest from './components/SlotTest.vue';
+component: {
+  NoSlotTest;
+}
+</script>
+
+<template>
+  <NoSlotTest />
+  <SlotTest />
+</template>
+
+<style scoped></style>
---
+++ front_오후/01_WEB/01_vue/0328_컴포넌트심화/slot-test/src/components/CheckBox2.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps(['id', 'checked']);
+const emit = defineEmits(['check-changed']);
+</script>
+
+<template>
+  <div>
+    <input
+      type="checkbox"
+      :value="id"
+      :checked="checked"
+      @change="emit('check-changed', { id, checked: $event.target.checked })"
+    />
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped></style>
