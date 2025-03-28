7898465ccd54e63c2ec28bc9e600357a05d5f463
kxxheehxxn <fpqk7099@gmail.com>
2025-03-28 17:52:12 +0900

    0328 마지막 수정해야함

---
+++ front_오후/01_WEB/01_vue/0328_컴포넌트심화/provide-inject-test/src/components/SongList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import SongListItem from './SongListItem.vue';
+
+defineProps(['songs', 'icons', 'doneCount']);
+</script>
+
+<template>
+  <ul>
+    <SongListItem
+      v-for="s in songs"
+      :key="s.id"
+      :song="s"
+      :icons="icons"
+      @update="updateDoneCount"
+    />
+  </ul>
+  <div>체크된 곡 수 : {{ doneCount }}</div>
+</template>
+
+<style scoped></style>
